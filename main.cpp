570ad12ac7a39f4399657a31b91e8933f1fa0ccb
ronczg <roncz.gabor1@gmail.com>
2024-09-05 13:52:27 +0200

    hibak kikommentezve

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //hibas konstans nev, N_ELEMENTS a helyes
+    std::cout << '1-100 ertekek duplazasa' //nincs ; a sor végén, és ' helyett "" kell
+    for (int i = 0;) // hianyzik a feltetel és a léptetés is
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // mindig hamis feltetel mert az i 0-ról kezd
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // nincs kiírandó érték + nincs ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // nincs inicializálva az atlag
+    for (int i = 0; i < N_ELEMENTS, i++) // a vessző helytelen, ; kell
     {
-        atlag += b[i]
+        atlag += b[i] //nincs pontosvessző 
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    //memoria nincs felszabadítva
     return 0;
 }
